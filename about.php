312f208acb13780762af5df750c0cb0185c5250c
sampanchan <spankratz@my.creativecircus.edu>
2021-10-21 13:07:30 -0400

    updated

--- about.php
+++ about.php
@@ -6,5 +6,28 @@ if (!isset($title)){
 <?php include('header.php'); ?>
 
     <h1>i am sam.</h1>
+    <div class="about">
+         <p >Pommy ipsum pot noodle baffled ended up brown bread chaps, 
+            jolly good oopsy-daisies throw a paddy knee high to a grasshopper 
+            i'll be a monkey's uncle. Farewell a week on Sunday doofer every 
+            fortnight what a doddle, marmite scrote. I could reet fancy a 
+            pennyboy collywobbles treacle the chippy River Song a right toff 
+            any road, best be off, brown sauce chap cheesed off grub's up a 
+            cracking got a lot of brass, scatterbrained lost her marbles could 
+            be a bit of a git naff 'ar kid a right royal knees up. Shakespeare 
+            ended up brown bread a total jessie punter atrocious, old chap 
+            scrote every fortnight The Hounds of Baskerville shepherd's pie, 
+        Queen Elizabeth and we all like figgy pudding what a load of cobblers.</p>
+        <p>River Song I could reet fancy a old chap middle class bog off golly gosh 
+            got his end away flabbergasted beefeater fried toast air one's dirty linen, 
+            pennyboy fried toast on his bill a right toff bow ties are cool on the pull 
+            jellied eels pork dripping. Sausage roll bit of alright golly upper class 
+            unhand me sir how's your father narky numpty bobby, throw a paddy have a 
+            gander down South pezzy little loo put me soaps back on. Queen Elizabeth 
+            it's me peepers Bob's your uncle crumpets rather, full English breakast 
+            tally-ho ee bah gum. A bottle of plonk on a stag do mince pies have a 
+            butcher's at this bloke, odds and sods bent as a nine bob note flog. </p>
+    </div>
+       
 
 <?php include('footer.php'); ?>
